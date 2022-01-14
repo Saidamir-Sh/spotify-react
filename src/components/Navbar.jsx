ede13674913efc83068c40f60bceee01ceb03769
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-14 17:30:15 +0500

    navbar links styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Logo from '../assets/Logo.png'
+import '../style/Navbar.css'
 import { Button } from 'react-bootstrap'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className='navbar vh-100 bg-black w-100 d-flex flex-column justify-content-between p-0' >
+        <div className='navbar vh-100  w-100 d-flex flex-column justify-content-between p-0'>
             <div>
-                <img src={Logo} style={{width: '7em'}}/>
-                <div><i class="bi bi-house-door-fill"></i> Home</div>
-                <div><i class="bi bi-search"></i> Search</div>
-                <div><i class="bi bi-collection"></i> Library</div>
+                <img src={Logo} style={{width: '7em'}} className='d-block py-3'/>
+                <div className='nav-link'><i class="bi bi-house-door-fill"></i> Home</div>
+                <div className='nav-link'><i class="bi bi-search"></i> Search</div>
+                <div className='nav-link'><i class="bi bi-collection"></i> Library</div>
             </div>
             <div>
                 <Button variant="light" style={{borderRadius: '15px'}}>Sign up</Button>
