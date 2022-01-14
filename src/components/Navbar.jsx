f8730cefd0de5c3bf79cd1545700aef6c89bed38
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-14 18:03:18 +0500

    player finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,20 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className='navbar vh-100  w-100 d-flex flex-column justify-content-between p-0'>
+        <div className='navbar w-100 d-flex flex-column justify-content-between p-0' style={{height: '90vh'}}>
             <div>
                 <img src={Logo} style={{width: '7em'}} className='d-block py-3'/>
                 <div className='nav-link'><i class="bi bi-house-door-fill"></i> Home</div>
                 <div className='nav-link'><i class="bi bi-search"></i> Search</div>
                 <div className='nav-link'><i class="bi bi-collection"></i> Library</div>
             </div>
-            <div>
-                <Button variant="light" style={{borderRadius: '15px'}}>Sign up</Button>
+            <div className='d-flex flex-column justify-content-center' style={{width: '85%'}}>
+                <Button variant="light mb-3" style={{borderRadius: '15px'}}>Sign up</Button>
                 <Button variant="outline-secondary">Login</Button>
+                <div className='footer-link mx-auto'>
+                    <a className='text-muted'>Cookie | </a>
+                    <a className='text-muted'>Privacy Policy</a>
+                </div>
             </div>
         </div>
     )
